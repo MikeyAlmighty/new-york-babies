3d57562fc83a16a0034ff9477ab9a9ec8d18ab10
MikeyAlmighty <mike.leibbrandt@gmail.com>
2024-03-19 19:44:57 +0200

    Init

---
+++ src/test/java/com/example/newyorkbabies/NewYorkBabiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newyorkbabies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYorkBabiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
