ad9eca67a97279c04432ba175aa42ac10bb80746
MikeyAlmighty <mike.leibbrandt@gmail.com>
2024-03-31 09:08:07 +0200

    WIP Initialized FrontEnd

---
+++ new-york-babies-web/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { BasicTable } from "./components/BasicTable";
+import "./styles.css"
+
+export const App = () => (
+    <div style={{ textAlign: "center" }}>
+        <h1>New York Baby Name Index</h1>
+        <BasicTable />
+    </div>
+)
---
+++ new-york-babies-web/src/components/Columns.ts
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: "First Name",
+        accessor: "firstName"
+    },
+    {
+        Header: "Year Of Birth",
+        accessor: "yearOfBirth"
+    },
+    {
+        Header: "Gender",
+        accessor: "gender"
+    },
+    {
+        Header: "Ethnicity",
+        accessor: "ethnicity"
+    },
+    {
+        Header: "Count",
+        accessor: "count"
+    },
+    {
+        Header: "Score",
+        accessor: "score"
+    }
+]
